b138e795ebd83786a91bd7cd5227e464ec4bf57f
thYun <wingindream@gmail.com>
2021-10-06 00:38:36 +0900

    Initial commit

---
+++ src/main/kotlin/kr/co/ats/camping/CampingApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.ats.camping
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CampingApplication
+
+fun main(args: Array<String>) {
+    runApplication<CampingApplication>(*args)
+}
---
+++ src/test/kotlin/kr/co/ats/camping/CampingAtsApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.ats.camping
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CampingAtsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
